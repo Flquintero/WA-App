e089a5e2af4c4d909e28800e63294744e8cc79a8
Francisco Quintero <flquintero1@gmail.com>
2024-03-01 21:47:32 -0500

    chore: abstracted recipe details

---
+++ frontend/components/pages/recipes-single/RecipeDetails.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="single-recipe-details">
+        <h1 class="single-recipe-details__name">
+            {{ currentRecipe.name }}
+        </h1>
+        <h4 class="single-recipe-details__email">
+            <ClientOnly
+                ><font-awesome-icon :icon="['fas', 'envelope']"
+            /></ClientOnly>
+            {{ currentRecipe.author_email }}
+        </h4>
+        <p class="single-recipe-details__description">
+            {{ currentRecipe.description }}
+        </p>
+    </div>
+</template>
+<script setup lang="ts">
+// Props
+const props = defineProps({
+    currentRecipe: {
+        type: Object, // add typing,
+    },
+});
+const { currentRecipe } = props;
+</script>
+<style lang="scss">
+.single-recipe-details {
+    &__name {
+        text-transform: capitalize;
+        padding-bottom: pxToRem(10);
+        border-bottom: 1px solid $border-color;
+    }
+    &__email {
+        margin-top: pxToRem(20);
+        svg {
+            color: $primary;
+        }
+    }
+    &__description {
+        margin-top: pxToRem(20);
+        background-color: $hover-color;
+        padding: pxToRem(20);
+        border-radius: $border-radius;
+    }
+}
+</style>
--- frontend/pages/recipes/[slug].vue
+++ frontend/pages/recipes/[slug].vue
@@ -8,18 +8,8 @@
         </NuxtLink>
         <div v-if="currentRecipe" class="single-recipe-view__content">
             <BaseImageGallery :recipe-images="currentRecipe.images" />
-            <h1 class="single-recipe-view__content-name">
-                {{ currentRecipe.name }}
-            </h1>
-            <h4 class="single-recipe-view__content-email">
-                <ClientOnly
-                    ><font-awesome-icon :icon="['fas', 'envelope']"
-                /></ClientOnly>
-                {{ currentRecipe.author_email }}
-            </h4>
-            <p class="single-recipe-view__content-description">
-                {{ currentRecipe.description }}
-            </p>
+            <SingleRecipeDetails :current-recipe="currentRecipe" />
+            <BaseTabsMenu :tab-options="['Ingredients', 'Steps']" />
         </div>
     </main>
 </template>
@@ -62,15 +52,6 @@ onMounted(getCurrentRecipe);
         flex-direction: column;
         max-width: pxToRem(800);
         margin: pxToRem(30) auto;
-        &-name {
-            text-transform: capitalize;
-        }
-        &-email {
-            margin-top: pxToRem(10);
-        }
-        &-description {
-            margin-top: pxToRem(20);
-        }
     }
 }
 </style>
